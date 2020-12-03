afe8674f04a72aa36e7b4bafc435eca5306d3702
ridkeim <ridkeim@gmail.com>
2020-12-03 12:27:12 +0300

    реализовано сохранение в файл и чтение настроек параметров текста

--- app/src/main/java/ru/ridkeim/settingsexample/MainActivity.kt
+++ app/src/main/java/ru/ridkeim/settingsexample/MainActivity.kt
@@ -4,24 +4,36 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.SharedPreferences
 import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.TextView
 import androidx.core.graphics.ColorUtils
 import androidx.preference.PreferenceManager
+import java.io.BufferedReader
+import java.io.FileNotFoundException
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 
 class MainActivity : AppCompatActivity(){
 
-    private lateinit var textView: TextView
+    companion object {
+        private const val FILENAME = "sample.txt"
+        private val TAG = MainActivity::class.qualifiedName
+    }
+
+    private lateinit var textEdit: TextView
     private lateinit var prefs: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         prefs = PreferenceManager.getDefaultSharedPreferences(this)
-        textView = findViewById(R.id.textView)
+        textEdit = findViewById(R.id.textEdit)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -31,15 +43,30 @@ class MainActivity : AppCompatActivity(){
 
     override fun onResume() {
         super.onResume()
+        if(prefs.getBoolean(getString(R.string.pref_open_mode),false)){
+            openFile()
+        }
         updateTextColor()
+        updateTextSize()
+        updateStyle()
+    }
+
+    private fun updateStyle() {
+        val typeface = when (prefs.getString(getString(R.string.pref_text_style), "")) {
+            "bold" -> Typeface.BOLD
+            "bold_italic" -> Typeface.BOLD_ITALIC
+            "italic" -> Typeface.ITALIC
+            else -> Typeface.NORMAL
+        }
+        textEdit.setTypeface(null,typeface)
     }
 
     private fun updateTextColor(){
         val keyOfColors = mapOf(
-            "text_color_black" to Color.BLACK,
-            "text_color_red" to Color.RED,
-            "text_color_green" to Color.GREEN,
-            "text_color_blue" to Color.BLUE
+            getString(R.string.pref_text_color_black) to Color.BLACK,
+            getString(R.string.pref_text_color_red) to Color.RED,
+            getString(R.string.pref_text_color_green) to Color.GREEN,
+            getString(R.string.pref_text_color_blue) to Color.BLUE
         )
         var currentColor = Color.BLACK
         var colorsCount = 0
@@ -57,17 +84,28 @@ class MainActivity : AppCompatActivity(){
             }
         }
         if(colorsCount == 0){
-            textView.setTextColor(R.attr.colorOnPrimary)
+            textEdit.setTextColor(R.attr.colorOnPrimary)
         }else{
-            textView.setTextColor(currentColor)
+            textEdit.setTextColor(currentColor)
         }
+    }
 
+    private fun updateTextSize(){
+        val sizeDefValueString = getString(R.string.text_size_def_value)
+        val sizeDefValueFloat = sizeDefValueString.toFloat()
+        val prefTextSize = try {
+            prefs.getString(getString(R.string.pref_text_size),sizeDefValueString)?.toFloat() ?: sizeDefValueFloat
+        } catch (e : NumberFormatException){
+            prefs.edit().putString(getString(R.string.pref_text_size),sizeDefValueString).apply()
+            sizeDefValueFloat
+        }
+        textEdit.textSize = prefTextSize
     }
 
     @SuppressLint("QueryPermissionsNeeded")
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
-            R.id.settingsMenuItem -> {
+            R.id.action_settings -> {
                 Intent(this, SettingsActivity::class.java).also {
                    it.resolveActivity(packageManager)?.also { _ ->
                        startActivity(it)
@@ -75,7 +113,36 @@ class MainActivity : AppCompatActivity(){
                 }
                 return true
             }
+            R.id.action_open -> openFile()
+            R.id.action_save -> saveFile()
         }
         return super.onOptionsItemSelected(item)
     }
+
+    private fun saveFile() {
+        try {
+            OutputStreamWriter(openFileOutput(FILENAME, 0)).use {
+                it.write(textEdit.text.toString())
+            }
+        } catch (e : FileNotFoundException){
+            e.printStackTrace()
+            Log.d(TAG, "file $FILENAME not found")
+        }
+    }
+
+    private fun openFile() {
+        try {
+            val stringBuilder = StringBuilder()
+            BufferedReader(InputStreamReader(openFileInput(FILENAME))).useLines {
+                it.forEach { line ->
+                    stringBuilder.append("$line\n")
+                }
+            }
+            textEdit.text = stringBuilder
+        } catch (e : FileNotFoundException) {
+            e.printStackTrace()
+            Log.d(TAG, "file $FILENAME not found")
+        }
+    }
+
 }
